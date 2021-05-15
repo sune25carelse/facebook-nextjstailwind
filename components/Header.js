bf0f7c6e48222b01d744fbe39d3eefd2d65e9668
Sune Carelse <sune.carelse@takealot.com>
2021-05-15 22:02:55 +0200

    header search corrected

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+
+import {
+  FLagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
 function Header() {
-  return <header>
-      
-  </header>;
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full ">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  );
 }
 
 export default Header;
