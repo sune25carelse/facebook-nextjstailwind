150ba1405480e06c39f522722dc611f18c83a351
Sune Carelse <sune.carelse@takealot.com>
2021-05-16 15:46:20 +0200

    header icons and search left/center styled

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 import {
   BellIcon,
   ChatIcon,
@@ -9,17 +10,16 @@ import {
 } from "@heroicons/react/solid";
 
 import {
-  FLagIcon,
+  FlagIcon,
   PlayIcon,
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
-
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -28,12 +28,26 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <div className="flex ml-2 items-center rounded-full ">
-          <SearchIcon className="h-6" />
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600 " />
+          <input
+            className="flex ml-2 items-center
+            bg-transparent outline-none placeholder-gray-500 "
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
       {/* Center */}
+      <div className="flex justify-center flex-grow ">
+        <div className="flex space-x-6 md:space-x-2 ">
+          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
     </div>
