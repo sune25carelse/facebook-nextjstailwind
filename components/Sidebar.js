d821f5176bfdddafb0bb0b720577cd0fd1268de5
Sune Carelse <sune.carelse@takealot.com>
2021-06-24 21:38:48 +0200

    sidebar icons created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,30 @@
+import { useSession } from "next-auth/client";
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline";
+import {
+  CalendarIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+
 function Sidebar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [session, loading] = useSession();
+  return (
+    <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow src={session.user.image} title={sessio.user.name} />
+      <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="MarketPlace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
