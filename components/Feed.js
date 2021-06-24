4086f1deff2096a1da76d9d29faadb19a21b30a4
Sune Carelse <sune.carelse@takealot.com>
2021-06-24 22:34:10 +0200

    feed stories updated

--- components/Feed.js
+++ components/Feed.js
@@ -2,11 +2,12 @@ import Stories from "./Stories";
 
 function Feed() {
   return (
-    <div className="flex-grow h-screen pb-44 pt-6">
-      {/* Stories */}
-      <Stories />
-      {/* inputBox */}
-      {/* Posts */}
+    <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+        <Stories />
+        {/* inputBox */}
+        {/* Posts */}
+      </div>
     </div>
   );
 }
