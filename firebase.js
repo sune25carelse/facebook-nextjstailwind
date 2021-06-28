00d0176ca3f79ea14206cf6bad2e58880d93c70b
Sune Carelse <sune.carelse@takealot.com>
2021-06-28 20:55:38 +0200

    yarn add firebase and import firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwtaFLpd-tmSWg28dFBINyUHBDhfcVVs8",
+  authDomain: "fb-nextjstailwnd.firebaseapp.com",
+  projectId: "fb-nextjstailwnd",
+  storageBucket: "fb-nextjstailwnd.appspot.com",
+  messagingSenderId: "528447397150",
+  appId: "1:528447397150:web:d5e842adc378a7b97cbd0f",
+};
