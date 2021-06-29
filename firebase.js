fd93a61539ba4d4e7aff382fbac60c112d2af838
Sune Carelse <sune.carelse@takealot.com>
2021-06-29 21:04:11 +0200

    firebase db /storage app initialize created

--- firebase.js
+++ firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+import "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBwtaFLpd-tmSWg28dFBINyUHBDhfcVVs8",
   authDomain: "fb-nextjstailwnd.firebaseapp.com",
@@ -6,3 +9,12 @@ const firebaseConfig = {
   messagingSenderId: "528447397150",
   appId: "1:528447397150:web:d5e842adc378a7b97cbd0f",
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const storage = firebase.storage();
+
+export { db, storage };
