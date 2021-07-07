56c79580c57e5125a6f5738f729ed3de293ce1db
Sune <sacarelse@gmail.com>
2021-07-07 21:10:16 +0200

    updated

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
+
 export default function Home({ session }) {
   if (!session) return <Login />;
 
